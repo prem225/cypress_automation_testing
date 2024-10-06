e54fef36af580e08f476ea0bca49eb8702997c0f
prem225 <prem.mmr2000@gmail.com>
2024-10-07 00:30:20 +0530

    removed failed testcases

--- cypress/e2e/APItesting/dbtest.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("database testing",()=>{
-    it("validate columns",()=>{
-        cy.task('queryDb','select * from new_schema.mock_data where id=1').then((result: any)=>{
-            expect(result[0].first_name).to.equal("Doyle")
-            expect(result[0].last_name).to.equal("Pardey")
-            expect(result[0].email).to.equal("dpardey0@dropbox.com")    
-        })
-    })
-})
